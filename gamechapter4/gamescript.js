170d20eb56864d3ec857aeb266f4e3e1554582d6
Rei Greiq <greiqrei@gmail.com>
2022-06-13 22:33:26 +0900

    update all

--- gamechapter4/gamescript.js
+++ gamechapter4/gamescript.js
@@ -2,19 +2,20 @@
 let player1 = document.getElementsByClassName("player1")
 let player2 = document.getElementsByClassName("bot");
 let reset = document.getElementById("refresh");
-
-
+let scorplayer1 = document.getElementsByClassName("pemain11");
+let scorcom = document.getElementsByClassName("com");
 class dataawal{ //CLASS DATA AWAL YG SIAP DIPAKE
     static keterangandraw = "DRAW";
 }
 class datapemain{ //CLASS CONSTRUCTOR DATA PLAYER 1 DAN BOT
-    constructor(indexplayer,keteranganmenang){
+    constructor(indexplayer,keteranganmenang,scor){
         this.indexplayer = indexplayer;
         this.keteranganmenang = keteranganmenang;
+        this.scor = scor;
     }
 }
-let pemain1 = new datapemain(0,"PLAYER 1 WIN"); //OBJEK PEMAIN 1
-let bot1 = new datapemain(0,"COM WIN") //OBJEK BOT
+let pemain1 = new datapemain(0,"PLAYER 1 WIN",0); //OBJEK PEMAIN 1
+let bot1 = new datapemain(0,"COM WIN",0) //OBJEK BOT
 
 function startgame(){
     for(let i = 1 ; i<player1.length ; i++){ //CEK CLIK MENGGUNAKAN ARRAY
@@ -28,6 +29,9 @@ function komputermemilih(i){ //KOMPUTER MEMILIH MENGGUNAKAN RANDOM MATH FLOOR
 }
 reset.onclick = function(){ //ONLCIK MEMANGGIL FUNCTION RESETBACKGROUND
     resetbackground();
+    pemain1.scor =0;
+    bot1.scor=0;
+    datascor(0,0);
 }
 function handleClick(i) { //SAAT DIKLIK MEMANGGIL RESET BACKGROUND, GANTI BACKGROUND 
     //DAN MENYIMPAN INDEXPLAYER AGAR DILAKUKAN LOGIKA PENGECEKAN
@@ -47,6 +51,7 @@ function resetbackground(){ //RESET BACKGROUND DENGAN ARRAY DAN PANJANG DARI JUM
     }
     pemenang.innerHTML = "V S";
     pemenang.setAttribute("style","background-color : none;color: #bd0102;");
+
 }
 function gantibackgroundbot(i){ //GANTI BACKGROUND MENGGUNAKAN ARRAY
     player2[i].setAttribute("style","background-color : #c4c4c4;border-radius: 15px;");
@@ -58,8 +63,13 @@ function setAttributemenang(){ //DATA PEMENANG DITAMPILAN
     pemenang.setAttribute
     ("style",
     "background-color : green;color: white;transform: rotate(-15deg);margin-left: 21%;width: 14vw;font-size: 3vw;border-radius: 5px");
+    datascor(pemain1.scor,bot1.scor);
 }
 
+function datascor(pemain1scor,bot1scor){
+    scorplayer1[0].innerHTML ="PLAYER 1 - SCOR : "+ pemain1scor;
+    scorcom[0].innerHTML ="COM - SCOR : "+ bot1scor;
+}
 function cekpemenang(indexplayer1, indexplayer2){ //LOGIKAN PENGECEKAN PEMENANGAN MENGGUNAKAN 2 INDEX DARI PLAYER 1 DAN BOT
     if(indexplayer1 == 1 && indexplayer2 ==1){
         pemenang.innerHTML = dataawal.keterangandraw;
@@ -68,14 +78,17 @@ function cekpemenang(indexplayer1, indexplayer2){ //LOGIKAN PENGECEKAN PEMENANGA
     }
     else if(indexplayer1 == 1 && indexplayer2 ==2){
         pemenang.innerHTML = bot1.keteranganmenang;
+        bot1.scor++;
         setAttributemenang();
     }
     else if(indexplayer1 == 1 && indexplayer2 ==3){
         pemenang.innerHTML =   pemain1.keteranganmenang;
+        pemain1.scor++;
         setAttributemenang();
     }
     else if(indexplayer1 == 2 && indexplayer2 ==1){
         pemenang.innerHTML =   pemain1.keteranganmenang;
+        pemain1.scor++;
         setAttributemenang();
     }
     else if(indexplayer1 == 2 && indexplayer2 ==2){
@@ -84,14 +97,17 @@ function cekpemenang(indexplayer1, indexplayer2){ //LOGIKAN PENGECEKAN PEMENANGA
     }
     else if(indexplayer1 == 2 && indexplayer2 ==3){
         pemenang.innerHTML =   bot1.keteranganmenang;
+        bot1.scor++;
         setAttributemenang();
     }
     else if(indexplayer1 == 3 && indexplayer2 ==1){
         pemenang.innerHTML =   bot1.keteranganmenang;
+        bot1.scor++;
         setAttributemenang();
     }
     else if(indexplayer1 == 3 && indexplayer2 ==2){
         pemenang.innerHTML =   pemain1.keteranganmenang;
+        pemain1.scor++;
         setAttributemenang();
     }
     else if(indexplayer1 == 3 && indexplayer2 ==3){
