cfea2917977c884093c77c7a53e0c8e60b73496b
Rei Greiq <greiqrei@gmail.com>
2022-06-15 11:23:25 +0900

    update all

--- gamechapter4/gamescript.js
+++ gamechapter4/gamescript.js
@@ -2,16 +2,16 @@
 let player1 = document.querySelectorAll(".player1");
 let player2 = document.querySelectorAll(".bot");
 let reset = document.getElementById("refresh");
-let scorPlayer1 = document.getElementsByClassName("pemain11");
-let scorCom = document.getElementsByClassName("com");
+let ScorePlayer1 = document.getElementsByClassName("pemain11");
+let ScoreCom = document.getElementsByClassName("com");
 class dataAwal{ //CLASS DATA AWAL YG SIAP DIPAKE
     static keteranganDraw = "DRAW";
 }
 class dataPemain{ //CLASS CONSTRUCTOR DATA PLAYER 1 DAN BOT
-    constructor(indexPlayer,keteranganMenang,scor){
+    constructor(indexPlayer,keteranganMenang,Score){
         this.indexPlayer = indexPlayer;
         this.keteranganMenang = keteranganMenang;
-        this.scor = scor;
+        this.Score = Score;
     }
 }
 let pemain1 = new dataPemain(0,"PLAYER 1 WIN",0); //OBJEK PEMAIN 1
@@ -29,9 +29,9 @@ function komputerMemilih(i){ //KOMPUTER MEMILIH MENGGUNAKAN RANDOM MATH FLOOR
 }
 reset.onclick = function(){ //ONLCIK MEMANGGIL FUNCTION resetBackground
     resetBackground();
-    pemain1.scor =0;
-    bot1.scor=0;
-    dataScor(0,0);
+    pemain1.Score =0;
+    bot1.Score=0;
+    dataScore(0,0);
 }
 function handleClick(i) { //SAAT DIKLIK MEMANGGIL RESET BACKGROUND, GANTI BACKGROUND 
     //DAN MENYIMPAN indexPlayer AGAR DILAKUKAN LOGIKA PENGECEKAN
@@ -63,12 +63,12 @@ function setAttributeMenang(){ //DATA PEMENANG DITAMPILAN
     pemenang.setAttribute
     ("style",
     "background-color : green;color: white;transform: rotate(-15deg);margin-left: 21%;width: 14vw;font-size: 3vw;border-radius: 5px");
-    dataScor(pemain1.scor,bot1.scor);
+    dataScore(pemain1.Score,bot1.Score);
 }
 
-function dataScor(pemain1scor,bot1scor){
-    scorPlayer1[0].innerHTML ="PLAYER 1-SCOR: "+ pemain1scor;
-    scorCom[0].innerHTML ="COM-SCOR: "+ bot1scor;
+function dataScore(pemain1Score,bot1Score){
+    ScorePlayer1[0].innerHTML ="PLAYER 1-Score: "+ pemain1Score;
+    ScoreCom[0].innerHTML ="COM-Score: "+ bot1Score;
 }
 function cekPemenang(indexPlayer1, indexPlayer2){ //LOGIKAN PENGECEKAN PEMENANGAN MENGGUNAKAN 2 INDEX DARI PLAYER 1 DAN BOT
     if(indexPlayer1 == 1 && indexPlayer2 ==1){
@@ -78,17 +78,17 @@ function cekPemenang(indexPlayer1, indexPlayer2){ //LOGIKAN PENGECEKAN PEMENANGA
     }
     else if(indexPlayer1 == 1 && indexPlayer2 ==2){
         pemenang.innerHTML = bot1.keteranganMenang;
-        bot1.scor++;
+        bot1.Score++;
         setAttributeMenang();
     }
     else if(indexPlayer1 == 1 && indexPlayer2 ==3){
         pemenang.innerHTML =   pemain1.keteranganMenang;
-        pemain1.scor++;
+        pemain1.Score++;
         setAttributeMenang();
     }
     else if(indexPlayer1 == 2 && indexPlayer2 ==1){
         pemenang.innerHTML =   pemain1.keteranganMenang;
-        pemain1.scor++;
+        pemain1.Score++;
         setAttributeMenang();
     }
     else if(indexPlayer1 == 2 && indexPlayer2 ==2){
@@ -97,17 +97,17 @@ function cekPemenang(indexPlayer1, indexPlayer2){ //LOGIKAN PENGECEKAN PEMENANGA
     }
     else if(indexPlayer1 == 2 && indexPlayer2 ==3){
         pemenang.innerHTML =   bot1.keteranganMenang;
-        bot1.scor++;
+        bot1.Score++;
         setAttributeMenang();
     }
     else if(indexPlayer1 == 3 && indexPlayer2 ==1){
         pemenang.innerHTML =   bot1.keteranganMenang;
-        bot1.scor++;
+        bot1.Score++;
         setAttributeMenang();
     }
     else if(indexPlayer1 == 3 && indexPlayer2 ==2){
         pemenang.innerHTML =   pemain1.keteranganMenang;
-        pemain1.scor++;
+        pemain1.Score++;
         setAttributeMenang();
     }
     else if(indexPlayer1 == 3 && indexPlayer2 ==3){
